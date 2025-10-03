64b9800d1b1d70ba7e6bacf223f87ea6e6d9956b
Ola <olaeniola16@gmail.com>
2025-10-03 17:47:20 +0000

    Modified files modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,33 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, Ola is a better DevOps engineer than all the DevOps engineers currently working with Facebook. He is determined");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.start();
+        System.out.println("Server started at http://0.0.0.0:8080");
+
+        // Prevent program from exiting immediately
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello, Ola is a better DevOps engineer!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
+
